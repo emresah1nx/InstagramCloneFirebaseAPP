4b69f76201df510a1126248cc0c303aeafd75a42
emresahin <aaemresahinn@gmail.com>
2024-05-22 02:34:21 +0300

    Signup-in-v0.2

--- firebaseInstaCloneApp/ViewController.swift
+++ firebaseInstaCloneApp/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
@@ -22,6 +24,27 @@ class ViewController: UIViewController {
     }
     
     @IBAction func signUpClicked(_ sender: Any) {
+        
+        if emailText.text != "" && passwordText.text != ""  {
+            
+            Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { (Authdata, error) in
+              if error != nil {
+                  self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
+                    
+                } else {
+                    self.performSegue(withIdentifier: "toTabBar", sender: nil)
+                }
+            }
+        } else {
+            makeAlert(titleInput: "Error!", messageInput: "Username/Password?")
+        }
+    }
+    func makeAlert(titleInput:String , messageInput:String)   {
+        
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
